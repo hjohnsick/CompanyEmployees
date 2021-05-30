5bc4afcbdc0c0c435593ccc6735c2ad942535bab
heather johnsick <hjohnsick@yahoo.com>
2021-05-30 11:01:39 -0400

    set up get request

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //_repository.Company.AnyMethodFromCompanyRepository();
+            //_repository.Employee.AnyMethodFromEmployeeRepository();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
